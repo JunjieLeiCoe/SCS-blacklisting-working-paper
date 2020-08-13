203539de171299d0addf9c1e500518086184e0b3
JunjieLei <jlei@coe.edu>
2020-08-13 13:01:47 -0700

    connect MySQL sever and Rstudio

---
+++ MySQL_shixin.R
@@ -0,0 +1,21 @@
+# install.packages('RMySQL')
+library(RMySQL)
+library(DBI)
+
+myDB <-  dbConnect(
+  MySQL(), 
+  user = 'root',
+  host = 'localhost', 
+  password = 'lei19961001',
+  port = 3306, 
+  dbname = 'shiXinDB'
+)
+
+dbListTables(myDB)
+dbListFields(myDB, 'sx')
+
+# Set the encoding for Rstudio
+rs <- dbSendQuery(myDB, 'SET NAMES utf8')   
+s ydata <- dbGetQuery(myDB, "select * from sx")
+
+data
