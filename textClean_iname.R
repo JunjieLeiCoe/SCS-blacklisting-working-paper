c881d289923a479c321286e1fffe2430478d88cc
JunjieLei <jlei@coe.edu>
2021-05-05 23:01:39 -0700

    textClean_iname

--- textClean_iname.R
+++ textClean_iname.R
@@ -1,5 +1,3 @@
-
-
 getwd()
 # reset wd;
 setwd("~/JUNJIE/WebScraper_Research2020S")
@@ -15,19 +13,19 @@ text <- read.csv("./data/sh_corp.csv")
 
 ## We only select a sub sample;
 ## To reduce Computing time;
-# text <- text %>% sample_n(2000)
-text <- text[,c("shixin_id","duty")]
+## text <- text %>% sample_n(2000)
+text <- text[,c("shixin_id","iname")]
 
 ## duty col --> removing HTML; Emotion; Special Chrs
-text$duty <-  text$duty %>% replace_html() %>% replace_emoticon() %>% 
+text$name <-  text$iname %>% replace_html() %>% replace_emoticon() %>% 
   replace_tag() %>% gsub("[a-zA-Z0-9\\.^_ㄟ|(|∞|﹏|．| ～|-。“”！!]","",.)
 
 ## Only select the texts with more than 20 wds
-text <- subset(text, nchar(text$duty) > 20 )
+text <- subset(text, nchar(text$name) > 4 )
 
 ## 同时删除了数字;
 x <- "a1~!@#$%^&*(){}_+:\"<>?,./;'[]-=" 
-text$duty <- str_replace_all(text$duty, "[[:punct:]]", " ")
+text$name <- str_replace_all(text$name, "[[:punct:]]", " ")
 
 # --------- 可 output raw data HERE --------
 ##
@@ -40,19 +38,20 @@ text$duty <- str_replace_all(text$duty, "[[:punct:]]", " ")
 ## 分词
 library(jiebaR)
 wk <-worker("mix") #建立模型分词
-words <- segment(text$duty, wk)
+words <- segment(text$name, wk)
 
 tab <- table(words)
 tab <- sort(tab, decreasing = TRUE)
 
 ## 去掉单个字的词语
-tab2 <- tab[stringr::str_length(names(tab)) > 2]
+tab2 <- tab[stringr::str_length(names(tab)) > 1]
+
+
 
 ## 发现常用 停止词;
-knitr::kable(as.data.frame((tab2)))
+head(knitr::kable(as.data.frame((tab2))), 100)
 
-
-t <- readLines('data/stopwords.txt')
+t <- readLines('data/stopwords_name.txt')
 stopwords <- c(NULL)
 
 for (i in 1:length(t)){
@@ -69,13 +68,20 @@ freq <- sort(
   decreasing = T
 )
 
-freq_head <- freq[stringr::str_length(names(freq)) > 2]
-pie(head(freq_head,10),family='STXihei')
+freq_head <- freq[stringr::str_length(names(freq)) > 1]
+pie(head(freq_head,15),family='STXihei')
 
-freq_tail <- freq[stringr::str_length(names(freq)) > 3]
+freq_head <- freq[stringr::str_length(names(freq)) > 2]
+pie(head(freq_head,15),family='STXihei')
+
+
+
+freq_tail <- freq[stringr::str_length(names(freq)) > 2]
 pie(tail(freq_tail, 10), family="STXihei")
 
-freq_wcloud <- freq[stringr::str_length(names(freq)) >2]
+
+
+freq_wcloud <- freq[stringr::str_length(names(freq)) > 2]
 
 mydata=data.frame(word=names(freq_wcloud),
                   freq=as.vector(freq_wcloud),
